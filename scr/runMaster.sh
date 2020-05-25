fcf7d299d9f567c60af32797175976274cf7a965
Joel Ströbaek <joel.strobaek@gmail.com>
2020-05-25 21:13:31 +0200

    Update runTest.sh and runMaster.sh with current code

--- scr/runMaster.sh
+++ scr/runMaster.sh
@@ -1,18 +1,164 @@
-# Includes commands used for verifying that a QIIME pipeline
-# equivalent can be performed in QIIME 2.
+# The QIIME 2 pipeline implemented as a suggested replacement to the current
+# QIIME 1 pipeline in use at GML, at Lund University in Sweden.
+#
+# Please see the results-directory README for information on directory setup.
 
 
-    # DATA IMPORT
+# DATA IMPORT
+#
+# Import pre-formated metadata-file (see runTest.sh for formating details).
+qiime metadata tabulate \
+  --m-input-file "$PWD"/data/metadata.tsv \
+  --o-visualization "$PWD"/rst/00-import/metadata.qzv
 
-# ...
+# View metadata.
+qiime tools view "$PWD"/rst/00-import/metadata.qzv
+
+# Import FASTQ.gz-files to QIIME 2 data artifact (.qza).
+# The type-flag is used to specify the nature of the data to be imported. Where
+# the current argument marks the data as FASTQ-files of the Casava 1.8
+# demultiplexed format.
+# More info here: https://docs.qiime2.org/2020.2/tutorials/importing/
+qiime tools import \
+  --type 'SampleData[PairedEndSequencesWithQuality]' \
+  --input-path "$PWD"/data/samples-fastq-gz \
+  --input-format CasavaOneEightSingleLanePerSampleDirFmt \
+  --output-path "$PWD"/rst/00-import/demux-paired-end.qza
+
+# Summarize import output to QIIME 2 visualization artifact (.qzv)
+qiime demux summarize \
+  --i-data "$PWD"/rst/00-import/demux-paired-end.qza \
+  --o-visualization "$PWD"/rst/00-import/demux-paired-end.qzv
+
+# View the output.
+qiime tools view "$PWD"/rst/00-import/demux-paired-end.qzv
 
 
-    # DEMULTIPLEXING?
+# DEMULTIPLEXING--omitted
+#
+# The data used to perform this analysis was subject to demultiplexing, and
+# quality control (QC) prior to import into QIIME 2.
 
-# ...
+
+# DENOISE AND JOIN
+#
+# Apply DADA2, where all cores are used when --p-n-threads is set to 0. As per
+# plugin requirements, primers need to be removed.
+# Outputs ASV feature table, and rep-seqs.
+qiime dada2 denoise-paired \
+  --i-demultiplexed-seqs "$PWD"/rst/00-import/demux-paired-end.qza \
+  --p-trunc-len-f 250 \
+  --p-trim-left-f 22 \
+  --p-trunc-len-r 250 \
+  --p-trim-left-r 22 \
+  --p-n-threads 0 \
+  --o-table "$PWD"/rst/01-denoise-and-join/joined-table.qza \
+  --o-representative-sequences \
+  "$PWD"/rst/01-denoise-and-join/joined-rep-seqs.qza \
+  --o-denoising-stats "$PWD"/rst/01-denoise-and-join/denoising-stats.qza
+
+# Summarize primary DADA2 output.
+qiime feature-table summarize \
+  --i-table "$PWD"/rst/01-denoise-and-join/joined-table.qza \
+  --o-visualization "$PWD"/rst/01-denoise-and-join/joined-table.qzv \
+  --m-sample-metadata-file "$PWD"/data/metadata.tsv
+
+qiime feature-table tabulate-seqs \
+  --i-data "$PWD"/rst/01-denoise-and-join/joined-rep-seqs.qza \
+  --o-visualization "$PWD"/rst/01-denoise-and-join/joined-rep-seqs.qzv
+
+# View summaries.
+qiime tools view "$PWD"/rst/01-denoise-and-join/joined-table.qzv
+
+qiime tools view "$PWD"/rst/01-denoise-and-join/joined-rep-seqs.qzv
+
+# Visualize denoising statistics.
+qiime metadata tabulate \
+  --m-input-file "$PWD"/rst/01-denoise-and-join/denoising-stats.qza \
+  --o-visualization "$PWD"/rst/01-denoise-and-join/denoising-stats.qzv
+
+# View the output.
+qiime tools view "$PWD"/rst/01-denoise-and-join/denoising-stats.qzv
 
 
+# TAXONOMY CLASSIFICATION & TAXONOMIC ANALYSIS
+#
+# Generate tree for phylogenic diversity analysis.
+qiime phylogeny align-to-tree-mafft-fasttree \
+  --i-sequences "$PWD"/rst/01-denoise-and-join/joined-rep-seqs.qza \
+  --o-alignment "$PWD"/rst/02-phyl-tree/aligned-rep-seqs.qza \
+  --o-masked-alignment "$PWD"/rst/02-phyl-tree/masked-aligned-rep-seqs.qza \
+  --o-tree "$PWD"/rst/02-phyl-tree/unrooted-tree.qza \
+  --o-rooted-tree "$PWD"/rst/02-phyl-tree/rooted-tree.qza
 
-    # QUALITY CONTROL & FEATURE TABLE GENERATION
 
-# ...
+# BETA DIVERSITY ANALYSIS
+#
+# Apply diversity metrics to feature table.
+qiime diversity core-metrics-phylogenetic \
+  --i-phylogeny "$PWD"/rst/02-phyl-tree/rooted-tree.qza \
+  --i-table "$PWD"/rst/01-denoise-and-join/joined-table.qza \
+  --p-sampling-depth 5000 \
+  --m-metadata-file "$PWD"/data/metadata.tsv \
+  --output-dir "$PWD"/rst/03-diversity/core-metrics
+
+# Beta diversity based on permutation-based statistical test.
+# Treatment ONLY, as specified by --m-metadata-column flag.
+qiime diversity beta-group-significance \
+  --i-distance-matrix \
+  "$PWD"/rst/03-diversity/core-metrics/unweight_unifrac_distnc_matrx.qza \
+  --m-metadata-file "$PWD"/data/metadata.tsv \
+  --m-metadata-column Treatment \
+  --o-visualization \
+  "$PWD"/rst/03-diversity/unweight-unifrac-treat-sig.qzv \
+  --p-pairwise
+
+# To test continous metadata:
+# [...] you can use the qiime metadata distance-matrix in combination with
+# (qiime diversity mantel) and (qiime diversity bioenv) commands.
+
+
+# TAXONOMIC ANALYSIS
+#
+# Generate taxonomy with pre-trained Naive Bayes classifier, available here:
+# https://data.qiime2.org/2020.2/common/gg-13-8-99-515-806-nb-classifier.qza
+qiime feature-classifier classify-sklearn \
+  --i-classifier "$PWD"/rst/00-import/gg-13-8-99-515-806-nb-classifier.qza \
+  --i-reads "$PWD"/rst/01-denoise-and-join/joined-rep-seqs.qza \
+  --o-classification "$PWD"/rst/04-taxonomy/taxonomy.qza
+
+# Generate taxonomy table.
+qiime metadata tabulate \
+  --m-input-file "$PWD"/rst/04-taxonomy/taxonomy.qza \
+  --o-visualization "$PWD"/rst/04-taxonomy/taxonomy.qzv
+
+# View ouput.
+qiime tools view "$PWD"/rst/04-taxonomy/taxonomy.qzv
+
+# Generate interactive barplots.
+qiime taxa barplot \
+  --i-table "$PWD"/rst/01-denoise-and-join/joined-table.qza \
+  --i-taxonomy "$PWD"/rst/04-taxonomy/taxonomy.qza \
+  --m-metadata-file "$PWD"/data/metadata.tsv \
+  --o-visualization "$PWD"/rst/04-taxonomy/taxa-bar-plots.qzv
+
+# View ouput.
+qiime tools view "$PWD"/rst/04-taxonomy/taxa-bar-plots.qzv
+
+
+# DIFFERENTIAL ABUNDANCE (ANCOM)
+#
+# Generate compositional feature table artifact, by adding pseudocount.
+# qiime composition add-pseudocount \
+#   --i-table "$PWD"/rst/01-denoise-and-join/joined-table.qza \
+#   --o-composition-table "$PWD"/rst/05-ancom/comp-joined-table.qza
+#
+# Run ANCOM. This assumes that less than 25% of the features change between
+# groups (here Treatment).
+# qiime composition ancom \
+#   --i-table "$PWD"/rst/05-ancom/comp-joined-table.qza \
+#   --m-metadata-file "$PWD"/data/metadata.tsv \
+#   --m-metadata-column Treatment \
+#   --o-visualization "$PWD"/rst/05-ancom/ancom-treatment.qzv
+
+# 
--- scr/runTest.sh
+++ scr/runTest.sh
@@ -1,8 +1,12 @@
 # Includes commands used for verifying that a QIIME pipeline equivalent can be
-# performed in QIIME 2.
+# performed in QIIME 2. Prematurely ends after OTU feature tables are
+# generated and filtered, since a decision to focus on ASVs was made.
+# And, with the exeption of methods directly related to OTUs, the subsequent
+# analysis performed on the feature tables can be performed in a similar (or
+# identical) manner.
 
 
-# Commands could be run from a project directory of the following structure:
+# Commands where run from a project directory of the following structure:
   #  .
   #  ├── bin
   #  │  
@@ -28,19 +32,12 @@
   #  │   ├── 06-dn-clust
   #  │   ├── 07-feat-table-summary
   #  │   ├── 08-dn-chim-filtered
-  #  │   ├── 09-feat-freq-filtered
-  #  │   ├──
-  #  │   ├──
-  #  │   ├──
-  #  │   ├──
-  #  │   ├──
-  #  │   ├── 
-  #  │   └──
+  #  │   └── 09-feat-freq-filtered
   #  │  
   #  └── scr
 
 
-# RE-FORMAT MAPPING FILE
+# RE-FORMAT MAPPING FILE0
 #
 # ... to adhere to QIIME 2 header requirements:
 # https://docs.qiime2.org/2020.2/tutorials/metadata/
@@ -56,12 +53,14 @@
 #
 # Move specified FASTQ files to the QIIME 2 input folder, based on the
 # SampleID stored in the WN column of the mapping file.
-cut -f5 data/samples-origin/Joel_map.csv | tail -n+2 | \
+cut -f5 "$PWD"/data/samples-origin/Joel_map.csv | tail -n+2 | \
   while read ID; do \
-  mv data/samples-origin/"$ID"/Files/*.gz data/samples-fastq-gz/; done
+  mv \
+  "$PWD"/data/samples-origin/"$ID"/Files/*.gz "$PWD"/data/samples-fastq-gz/; \
+  done
 
 # Fetch relevant metadata from original mapping file.
-cut -f2,12,13,14,15,16,17,18,19,20 "$PWD"/data/samples-origin/Joel_map.csv \
+cut -f2,7,12,13,14,15,16,17,18,19,20 "$PWD"/data/samples-origin/Joel_map.csv \
   > "$PWD"/data/meta_tmp.tsv
 
 # Construct SampleID column with correct IDs.
@@ -69,7 +68,8 @@ echo 'SampleID' > data/id_tmp.tsv | cut -f5 data/samples-origin/Joel_map.csv | \
   tail -n+2 | \
   while read ID; do \
   grep 'Name:' data/samples-origin/"$ID"/SampleProperties | \
-  cut -d' ' -f2 >> data/id_tmp.tsv; done
+  cut -d' ' -f2 >> data/id_tmp.tsv; \
+  done
 
 # Join SampleID to metadata.
 paste data/id_tmp.tsv data/meta_tmp.tsv > data/metadata.tsv \
@@ -137,15 +137,16 @@ qiime vsearch cluster-features-de-novo \
   --o-clustered-table "$PWD"/rst/06-dn-clust/table-dn-99.qza \
   --o-clustered-sequences "$PWD"/rst/06-dn-clust/rep-seqs-dn-99.qza
 
-
 # Feature table summary.
 qiime feature-table summarize \
   --i-table "$PWD"/rst/06-dn-clust/table-dn-99.qza \
   --m-sample-metadata-file "$PWD"/data/metadata.tsv \
   --o-visualization "$PWD"/rst/07-prel-feat-table-summary/table-dn-99.qzv
 
-
-# Skipped this step due to lack of information...
+# Skipped this step due to lack of information, at the time. This could be
+# detrimental to the data analysis, depending on the upstream requirements of
+# the statistical tests applied.
+#
 # Filter out single sample features.
 # qiime feature-table filter-features \
 #   --i-table "$PWD"/rst/06-dn-clust/table-dn-99.qza \
@@ -165,7 +166,7 @@ qiime metadata tabulate \
   --m-input-file "$PWD"/rst/08-dn-chim-filtered/uchime-dn-check/stats.qza \
   --o-visualization "$PWD"/rst/08-dn-chim-filtered/uchime-dn-check/stats.qzv
 
-# Filter out chimeras AND "boarderline" chimeras.
+# Filter out chimeras AND "boarderline" chimeras from feature table.
 qiime feature-table filter-features \
   --i-table "$PWD"/rst/06-dn-clust/table-dn-99.qza \
   --m-metadata-file \
@@ -173,6 +174,12 @@ qiime feature-table filter-features \
   --o-filtered-table \
   "$PWD"/rst/08-dn-chim-filtered/uchime-dn-check/table-nonchim-wo-bl.qza
 
+# Visualize prior artifact output.
+qiime feature-table summarize \
+  --i-table "$PWD"/rst/08-dn-chim-filtered/table-nonchim-wo-bl.qza \
+  --o-visualization "$PWD"/rst/08-dn-chim-filtered/table-nonchim-wo-bl.qzv
+
+# Filter out chimeras AND "boarderline" chimeras from sequence artifact.
 qiime feature-table filter-seqs \
   --i-data "$PWD"/rst/06-dn-clust/rep-seqs-dn-99.qza \
   --m-metadata-file \
@@ -180,13 +187,20 @@ qiime feature-table filter-seqs \
   --o-filtered-data \
   "$PWD"/rst/08-dn-chim-filtered/uchime-dn-check/rep-seqs-nonchim-wo-bl.qza
 
-qiime feature-table summarize \
-  --i-table "$PWD"/rst/08-dn-chim-filtered/table-nonchim-wo-bl.qza \
-  --o-visualization "$PWD"/rst/08-dn-chim-filtered/table-nonchim-wo-bl.qzv
-
 # Remove singletons.
 qiime feature-table filter-features \
   --i-table "$PWD"/rst/08-dn-chim-filtered/table-nonchim-wo-bl.qza \
   --p-min-frequency 2 \
   --o-filtered-table \
-  "$PWD"/rst/09-feat-freq-filtered/feature-frequency-filtered-table.qza
+  "$PWD"/rst/09-feat-freq-filtered/feat-freq-filtered-table.qza
+
+# Visualize prior output.
+qiime feature-table summarize \
+  --i-table "$PWD"/rst/09-feat-freq-filtered/feat-freq-filtered-table.qza \
+  --o-visualization \
+  "$PWD"/rst/09-feat-freq-filtered/feat-freq-filtered-table.qzv
+
+# SCRIPT END
+#
+# No further efforts were spent on analysing OTUs, since it's largely abandoned
+# for QIIME 2, due to a growing body of support for the use of ASVs.
